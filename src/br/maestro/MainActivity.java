c361e7e26fc59cb715f1dc96cad4ccea9ecacfab
Rafael Monico <rmonico@moningrado>
2014-09-28 15:32:47 -0300

    ctrl+F

--- src/br/maestro/MainActivity.java
+++ src/br/maestro/MainActivity.java
@@ -8,32 +8,32 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-	private ListView drawerList;
+    private ListView drawerList;
 
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.mainactivity);
-		
-		initializeControls();
-		
-		setDrawerAdapter();
-	}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+
+        initializeControls();
+
+        setDrawerAdapter();
+    }
 
     private void initializeControls() {
-	    drawerList = (ListView) findViewById(R.id.mainactivity_drawerlist);
+        drawerList = (ListView) findViewById(R.id.mainactivity_drawerlist);
     }
 
     private void setDrawerAdapter() {
         ListAdapter adapter = new DrawerAdapter();
         drawerList.setAdapter(adapter);
     }
-    
+
     @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
