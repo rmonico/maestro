f8553dfa44408624c883a184f3855a12a099ac23
Rafael Monico <rmonico@moningrado>
2014-09-28 15:52:47 -0300

    Abrindo gaveta com botão da aplicação

--- src/br/maestro/MainActivity.java
+++ src/br/maestro/MainActivity.java
@@ -2,13 +2,18 @@ package br.maestro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
     private ListView drawerList;
+    private DrawerLayout mainActivityLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,12 @@ public class MainActivity extends Activity {
 
     private void initializeControls() {
         drawerList = (ListView) findViewById(R.id.mainactivity_drawerlist);
+        mainActivityLayout = (DrawerLayout) findViewById(R.id.mainactivity);
+
+        drawerToggle = new ActionBarDrawerToggle(this, mainActivityLayout, R.drawable.ic_launcher, R.string.mainactivity_draweropened, R.string.mainactivity_drawerclosed);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
     }
 
     private void setDrawerAdapter() {
@@ -34,6 +45,14 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
