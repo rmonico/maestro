fbf86adf2718bba4929269774e9ad6d0cc68d1ce
Rafael Monico <rmonico@moningrado>
2014-09-28 14:19:25 -0300

    Primeira versão do layout da gaveta de tags

---
+++ src/br/maestro/DrawerAdapter.java
@@ -0,0 +1,60 @@
+package br.maestro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+public class DrawerAdapter extends BaseAdapter implements ListAdapter {
+
+    private List<String> list;
+
+    public DrawerAdapter() {
+        list = createList();
+    }
+
+    private ArrayList<String> createList() {
+        final ArrayList<String> list = new ArrayList<String>();
+        
+        list.add("Hacklab");
+        list.add("Casa");
+        list.add("Saúde");
+
+        return list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View defaultItem = inflater.inflate(R.layout.mainactivity_drawerlistitem_default, parent, false);
+        
+        TextView label = (TextView) defaultItem.findViewById(R.id.mainactivity_drawerlistitem_label);
+        
+        label.setText(list.get(position));
+        
+        return defaultItem;
+    }
+
+}
--- src/br/maestro/MainActivity.java
+++ src/br/maestro/MainActivity.java
@@ -1,18 +1,35 @@
 package br.maestro;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-	@Override
+	private ListView drawerList;
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.mainactivity);
+		
+		initializeControls();
+		
+		setDrawerAdapter();
 	}
 
-	@Override
+    private void initializeControls() {
+	    drawerList = (ListView) findViewById(R.id.mainactivity_drawerlist);
+    }
+
+    private void setDrawerAdapter() {
+        ListAdapter adapter = new DrawerAdapter();
+        drawerList.setAdapter(adapter);
+    }
+    
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
