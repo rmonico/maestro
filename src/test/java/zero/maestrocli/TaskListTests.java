588c2ba39de987cd6be67ff9b1fd6e804bf4a48f
Rafael Monico <rmonico@moningrado>
2015-08-26 17:20:18 -0300

    TaskListTests.should_list_all_three_tasks: error in controller configuration

--- src/test/java/zero/maestrocli/TaskListTests.java
+++ src/test/java/zero/maestrocli/TaskListTests.java
@@ -1,13 +1,48 @@
 package zero.maestrocli;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import zero.easymvc.EasyMVC;
+import zero.easymvc.EasyMVCAssert;
+import zero.easymvc.EasyMVCException;
+import zero.maestro.model.Task;
 
 public class TaskListTests {
 
+    private EasyMVC controller;
+
+    @Before
+    public void setup() {
+        controller = new EasyMVC();
+
+        registerCommandsAndRenderers();
+    }
+
+    private void registerCommandsAndRenderers() {
+    }
+
     @Test
-    public void xx() {
-        assertTrue(false);
+    public void should_list_all_three_tasks() throws EasyMVCException {
+        List<Object> beans = controller.run("task", "ls");
+
+        EasyMVCAssert.assertBeanList(beans, 1);
+
+        @SuppressWarnings("unchecked")
+        List<Task> tasks = EasyMVCAssert.assertAndGetBean(beans, 0, List.class);
+
+        assertEquals(3, tasks.size());
+
+        Task task = tasks.get(0);
+
+        assertNotNull(task);
+        assertEquals("Primeira tarefa", task.getName());
+        assertNull(task.getSuperTask());
     }
 }
