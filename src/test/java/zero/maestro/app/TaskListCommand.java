db65235b8429a226e1da9189db5b0ae824fa4d6a
Rafael Monico <rmonico@moningrado>
2015-08-26 17:25:45 -0300

    Command: returning tasks beans

--- src/test/java/zero/maestro/app/TaskListCommand.java
+++ src/test/java/zero/maestro/app/TaskListCommand.java
@@ -1,8 +1,15 @@
 package zero.maestro.app;
 
+import java.util.List;
+
+import zero.easymvc.Bean;
 import zero.easymvc.CommandHandler;
+import zero.maestro.model.Task;
 
 public class TaskListCommand {
+
+    @Bean
+    private List<Task> tasks;
 
     @CommandHandler(path = { "task", "ls" })
     public void execute() {
--- src/test/java/zero/maestrocli/renderer/TaskListRenderer.java
+++ src/test/java/zero/maestrocli/renderer/TaskListRenderer.java
@@ -1,8 +1,15 @@
 package zero.maestrocli.renderer;
 
+import java.util.List;
+
+import zero.easymvc.Bean;
 import zero.easymvc.Renderer;
+import zero.maestro.model.Task;
 
 public class TaskListRenderer {
+
+    @Bean
+    private List<Task> tasks;
 
     @Renderer(path = { "task", "ls" })
     public void render() {
