0ed1e2014a6f00728222a802fecae1ebc0a03878
Rafael Monico <rmonico@moningrado>
2015-08-26 16:20:01 -0300

    Created empty test

---
+++ src/test/java/zero/maestrocli/TaskListTests.java
@@ -0,0 +1,13 @@
+package zero.maestrocli;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TaskListTests {
+
+    @Test
+    public void xx() {
+        assertTrue(false);
+    }
+}
