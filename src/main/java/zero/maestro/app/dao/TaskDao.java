c0927c4a9dd9795a82b09a3a470b1b83f24e41d5
Rafael Monico <rmonico@moningrado>
2015-09-21 06:23:47 -0300

    TaskDao created

---
+++ src/main/java/zero/maestro/app/dao/TaskDao.java
@@ -0,0 +1,13 @@
+package zero.maestro.app.dao;
+
+import java.util.List;
+
+import zero.maestro.model.Task;
+
+public class TaskDao {
+
+    public List<Task> queryForAll() {
+        return null;
+    }
+
+}
