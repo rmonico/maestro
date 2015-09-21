ad78435af4cdbe9b42de0f8bd11ff691afd4503b
Rafael Monico <rmonico@moningrado>
2015-09-21 08:12:19 -0300

    TaskList superTask tests (passing)

--- src/test/java/zero/maestrocli/TaskListTests.java
+++ src/test/java/zero/maestrocli/TaskListTests.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import zero.easymvc.EasyMVCAssert;
 import zero.easymvc.EasyMVCException;
 import zero.maestro.model.Task;
+import zero.utils.test.DBUnitDatasetFileNames;
 
 public class TaskListTests extends MaestrocliTest {
 
@@ -27,9 +28,25 @@ public class TaskListTests extends MaestrocliTest {
 
         assertEquals(3, tasks.size());
 
-        Assert.assertTask("First task ever", tasks.get(0));
-        Assert.assertTask("Second task", tasks.get(1));
-        Assert.assertTask("A third task", tasks.get(2));
+        Assert.assertTask("First task ever", null, tasks.get(0));
+        Assert.assertTask("Second task", null, tasks.get(1));
+        Assert.assertTask("A third task", null, tasks.get(2));
+    }
+
+    @Test
+    @DBUnitDatasetFileNames("dbunit/tasklisttests__should_list_task_with_supertask.xml")
+    public void should_list_task_with_supertask() throws EasyMVCException {
+        List<Object> beans = controller.run("task", "ls");
+
+        EasyMVCAssert.assertBeanList(beans, 1);
+
+        @SuppressWarnings("unchecked")
+        List<Task> tasks = EasyMVCAssert.assertAndGetBean(beans, 0, List.class);
+
+        assertEquals(2, tasks.size());
+
+        Assert.assertTask("A supertask", null, tasks.get(0));
+        Assert.assertTask("A task with supertask", "A supertask", tasks.get(1));
     }
 
 }
