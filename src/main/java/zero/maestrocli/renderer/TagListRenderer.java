df05309b10dd6d56bc675ac29e8d78ce0a97ff57
Rafael Monico <rmonico@moningrado>
2015-09-21 08:53:22 -0300

    TagListRenderer implementation

--- src/main/java/zero/maestrocli/renderer/TagListRenderer.java
+++ src/main/java/zero/maestrocli/renderer/TagListRenderer.java
@@ -1,9 +1,16 @@
 package zero.maestrocli.renderer;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import zero.easymvc.Renderer;
+import zero.listprinter.Column;
+import zero.listprinter.FormattedColumn;
+import zero.listprinter.IDFormatter;
+import zero.listprinter.ListPrinter;
 import zero.listprinter.ListPrinterException;
+import zero.listprinter.ReflectionFieldExtractor;
+import zero.listprinter.StringFormatter;
 import zero.maestro.model.Tag;
 
 public class TagListRenderer {
@@ -12,6 +19,23 @@ public class TagListRenderer {
 
     @Renderer(path = { "tag", "ls" })
     public void render() throws ListPrinterException {
+        ListPrinter printer = new ListPrinter();
 
+        printer.setEntityName("Tag", "Tags");
+
+        printer.setColumnDefinitions(createColumnDefinitions());
+
+        printer.setData(tags);
+
+        printer.print();
+    }
+
+    private List<Column> createColumnDefinitions() {
+        List<Column> defs = new LinkedList<>();
+
+        defs.add(new FormattedColumn("ID", new ReflectionFieldExtractor("id"), IDFormatter.getInstance()));
+        defs.add(new FormattedColumn("Name", new ReflectionFieldExtractor("name"), StringFormatter.getInstance()));
+
+        return defs;
     }
 }
