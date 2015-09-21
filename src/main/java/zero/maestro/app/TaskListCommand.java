1bdf28f7873a8cf1de8c4bee478554b0c8933b86
Rafael Monico <rmonico@moningrado>
2015-09-21 07:58:03 -0300

    TaskList command and renderer

--- src/main/java/zero/maestro/app/TaskListCommand.java
+++ src/main/java/zero/maestro/app/TaskListCommand.java
@@ -1,5 +1,6 @@
 package zero.maestro.app;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import zero.easymvc.Bean;
@@ -17,7 +18,7 @@ public class TaskListCommand {
     private List<Task> tasks;
 
     @CommandHandler(path = { "task", "ls" })
-    public void execute() {
+    public void execute() throws SQLException {
         tasks = dao.queryForAll();
     }
 }
--- src/main/java/zero/maestrocli/renderer/TaskListRenderer.java
+++ src/main/java/zero/maestrocli/renderer/TaskListRenderer.java
@@ -1,18 +1,43 @@
 package zero.maestrocli.renderer;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import zero.easymvc.Bean;
 import zero.easymvc.Renderer;
+import zero.listprinter.BooleanFormatter;
+import zero.listprinter.Column;
+import zero.listprinter.FormattedColumn;
+import zero.listprinter.IDFormatter;
+import zero.listprinter.ListPrinter;
+import zero.listprinter.ListPrinterException;
+import zero.listprinter.ReflectionFieldExtractor;
+import zero.listprinter.StringFormatter;
 import zero.maestro.model.Task;
 
 public class TaskListRenderer {
 
-    @Bean
     private List<Task> tasks;
 
     @Renderer(path = { "task", "ls" })
-    public void render() {
+    public void render() throws ListPrinterException {
+        ListPrinter printer = new ListPrinter();
 
+        printer.setEntityName("Task", "Tarefas");
+
+        printer.setColumnDefinitions(createColumnDefinitions());
+
+        printer.setData(tasks);
+
+        printer.print();
+    }
+
+    private List<Column> createColumnDefinitions() {
+        List<Column> defs = new LinkedList<>();
+
+        defs.add(new FormattedColumn("ID", new ReflectionFieldExtractor("id"), IDFormatter.getInstance()));
+        defs.add(new FormattedColumn("Nome", new ReflectionFieldExtractor("name"), StringFormatter.getInstance()));
+        defs.add(new FormattedColumn("Super task", new ReflectionFieldExtractor("superTask", new ReflectionFieldExtractor("name")), BooleanFormatter.getInstance()));
+
+        return defs;
     }
 }
