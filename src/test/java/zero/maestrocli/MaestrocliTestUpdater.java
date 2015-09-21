f87523cbee7505d38e665d1ffb616aebe10d0361
Rafael Monico <rmonico@moningrado>
2015-09-21 07:59:58 -0300

    MaestrocliTestUpdater

---
+++ src/test/java/zero/maestrocli/MaestrocliTestUpdater.java
@@ -0,0 +1,28 @@
+package zero.maestrocli;
+
+import zero.easymvc.ormlite.AbstractDatabaseVersion;
+import zero.easymvc.ormlite.DatabaseVersion;
+import zero.maestro.model.Task;
+
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class MaestrocliTestUpdater extends AbstractDatabaseVersion {
+
+    public MaestrocliTestUpdater(ConnectionSource connection, DatabaseVersion previousVersion) {
+        super(connection, previousVersion);
+    }
+
+    @Override
+    public void update() throws Exception {
+        TableUtils.dropTable(connection, Task.class, true);
+
+        super.update();
+    }
+
+    @Override
+    public int getCurrentVersion() {
+        return 0;
+    }
+
+}
