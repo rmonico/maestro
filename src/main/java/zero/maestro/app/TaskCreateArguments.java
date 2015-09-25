2af5950129012cd475b5131aa13083add9421fc5
Rafael Monico <rmonico@moningrado>
2015-09-25 13:48:36 -0300

    Arguments for task with super task

--- src/main/java/zero/maestro/app/TaskCreateArguments.java
+++ src/main/java/zero/maestro/app/TaskCreateArguments.java
@@ -1,11 +1,15 @@
 package zero.maestro.app;
 
 import zero.easymvc.PositionalParameter;
+import zero.easymvc.TokenParameter;
 
 public class TaskCreateArguments {
 
     @PositionalParameter
     private String taskName;
+
+    @TokenParameter(token = { "-s", "--supertaskid" })
+    private int superTaskID;
 
     public String getTaskName() {
         return taskName;
@@ -13,6 +17,14 @@ public class TaskCreateArguments {
 
     public void setTaskName(String taskName) {
         this.taskName = taskName;
+    }
+
+    public int getSuperTaskID() {
+        return superTaskID;
+    }
+
+    public void setSuperTaskID(int superTaskID) {
+        this.superTaskID = superTaskID;
     }
 
 }
