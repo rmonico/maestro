f985b0b43c7583111f74292149be8775dd0d19ed
Rafael Monico <rmonico@moningrado>
2015-09-25 12:22:31 -0300

    Created getters and setters for AttributeListArguments

--- src/main/java/zero/maestro/app/AttributeListArguments.java
+++ src/main/java/zero/maestro/app/AttributeListArguments.java
@@ -7,4 +7,12 @@ public class AttributeListArguments {
     @PositionalParameter
     private String tagName;
 
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
 }
