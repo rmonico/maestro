f56f11502eff22c000f3b56704a4685901f9a8f8
Rafael Monico <rmonico@moningrado>
2015-09-25 13:37:45 -0300

    Task create tests passing

--- src/main/java/zero/maestro/app/TaskCreateCommand.java
+++ src/main/java/zero/maestro/app/TaskCreateCommand.java
@@ -1,11 +1,18 @@
 package zero.maestro.app;
 
+import java.sql.SQLException;
+
 import zero.easymvc.ArgumentsBean;
 import zero.easymvc.Bean;
 import zero.easymvc.CommandHandler;
+import zero.easymvc.Dependency;
+import zero.maestro.app.dao.TaskDao;
 import zero.maestro.model.Task;
 
 public class TaskCreateCommand {
+
+    @Dependency
+    private TaskDao dao;
 
     @ArgumentsBean
     private TaskCreateArguments args;
@@ -14,10 +21,12 @@ public class TaskCreateCommand {
     private Task task;
 
     @CommandHandler(path = { "task", "add" })
-    public void execute() {
+    public void execute() throws SQLException {
         task = new Task();
 
-        task.setName("Some task");
+        task.setName(args.getTaskName());
+
+        dao.create(task);
     }
 
 }
