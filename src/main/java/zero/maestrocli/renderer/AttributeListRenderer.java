bd6f48c78381a30ff0eb63342ffbb1990d2b5434
Rafael Monico <rmonico@moningrado>
2015-09-25 13:28:18 -0300

    Renderer for list attribute

--- src/main/java/zero/maestrocli/renderer/AttributeListRenderer.java
+++ src/main/java/zero/maestrocli/renderer/AttributeListRenderer.java
@@ -1,6 +1,16 @@
 package zero.maestrocli.renderer;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import zero.easymvc.Renderer;
+import zero.listprinter.Column;
+import zero.listprinter.FormattedColumn;
+import zero.listprinter.IDFormatter;
+import zero.listprinter.ListPrinter;
+import zero.listprinter.ListPrinterException;
+import zero.listprinter.ReflectionFieldExtractor;
+import zero.listprinter.StringFormatter;
 import zero.maestro.model.Tag;
 
 public class AttributeListRenderer {
@@ -8,7 +18,26 @@ public class AttributeListRenderer {
     private Tag tag;
 
     @Renderer(path = { "attr", "ls" })
-    public void render() {
+    public void render() throws ListPrinterException {
+        ListPrinter printer = new ListPrinter();
 
+        printer.setEntityName("Attribute", "Attributes");
+
+        printer.setColumnDefinitions(createColumnDefinitions());
+
+        printer.setData(tag.getAttributes());
+
+        printer.print();
+    }
+
+    private List<Column> createColumnDefinitions() {
+        List<Column> defs = new LinkedList<>();
+
+        defs.add(new FormattedColumn("ID", new ReflectionFieldExtractor("id"), IDFormatter.getInstance()));
+        defs.add(new FormattedColumn("Name", new ReflectionFieldExtractor("name"), StringFormatter.getInstance()));
+        defs.add(new FormattedColumn("Type", new ReflectionFieldExtractor("type"), StringFormatter.getInstance()));
+        defs.add(new FormattedColumn("Tag", new ReflectionFieldExtractor("tag", new ReflectionFieldExtractor("name")), StringFormatter.getInstance()));
+
+        return defs;
     }
 }
