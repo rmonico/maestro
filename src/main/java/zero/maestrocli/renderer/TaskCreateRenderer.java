ad3a5e321671115090daf05c4c6cffe6d1f08d39
Rafael Monico <rmonico@moningrado>
2015-09-27 16:23:56 -0300

    Task created renderer

--- src/main/java/zero/maestrocli/renderer/TaskCreateRenderer.java
+++ src/main/java/zero/maestrocli/renderer/TaskCreateRenderer.java
@@ -9,6 +9,15 @@ public class TaskCreateRenderer {
 
     @Renderer(path = { "task", "add" })
     public void render() {
+        Task superTask = task.getSuperTask();
+
+        String message;
+        if (superTask == null)
+            message = String.format("New task created: \"%s\" (#%d)", task.getName(), task.getId());
+        else
+            message = String.format("New task created: \"%s\" -> \"%s\" (#%d)", superTask.getName(), task.getName(), task.getId());
+
+        System.out.println(message);
     }
 
 }
