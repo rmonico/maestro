70d9e13d4a237c9ae43870a6183f2396c65559a0
Rafael Monico <rmonico@moningrado>
2015-09-27 16:37:50 -0300

    Main

--- src/main/java/zero/maestrocli/Main.java
+++ src/main/java/zero/maestrocli/Main.java
@@ -1,7 +1,33 @@
 package zero.maestrocli;
 
+import zero.easymvc.EasyMVC;
+import zero.easymvc.StringArrayCommand;
+import zero.easymvc.ormlite.factory.AbstractApplicationFactory;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+
+        main.run(args);
     }
+
+    private void run(String[] args) throws Exception {
+        AbstractApplicationFactory factory = new MaestrocliApplicationFactory();
+
+        factory.makeProperties();
+
+        factory.makeLogger();
+
+        factory.makeConnectionManager();
+
+        factory.makeDaoManager();
+
+        EasyMVC controller = factory.makeController();
+
+        factory.checkAndUpdateDatabaseVersion();
+
+        controller.run(new StringArrayCommand(args));
+    }
+
 }
