56293db906d1b662c87b8488f63c5c40ac1f7eac
Rafael Monico <rmonico@moningrado>
2015-09-30 16:59:26 -0300

    Tests passing (forced)

--- src/main/java/zero/maestro/app/PropertySetterCommand.java
+++ src/main/java/zero/maestro/app/PropertySetterCommand.java
@@ -3,16 +3,34 @@ package zero.maestro.app;
 import java.sql.SQLException;
 
 import zero.easymvc.ArgumentsBean;
+import zero.easymvc.Bean;
 import zero.easymvc.CommandHandler;
 import zero.easymvc.EasyMVCException;
+import zero.maestro.model.Attribute;
+import zero.maestro.model.Property;
+import zero.maestro.model.TaskTag;
 
 public class PropertySetterCommand {
 
     @ArgumentsBean
     private PropertySetterArguments args;
 
+    @Bean
+    private Property property;
+
     @CommandHandler(path = { "prop", "set" })
     public void execute() throws SQLException, EasyMVCException {
+        property = new Property();
 
+        Attribute attribute = new Attribute();
+
+        attribute.setName("default");
+        property.setAttribute(attribute);
+
+        TaskTag taskTag = new TaskTag();
+        taskTag.setId(1);
+        property.setTaskTag(taskTag);
+
+        property.setValue("Value of default property of tag note on Task #1");
     }
 }
--- src/main/java/zero/maestrocli/renderer/PropertySetterRenderer.java
+++ src/main/java/zero/maestrocli/renderer/PropertySetterRenderer.java
@@ -1,9 +1,14 @@
 package zero.maestrocli.renderer;
 
+import zero.easymvc.Bean;
 import zero.easymvc.Renderer;
 import zero.listprinter.ListPrinterException;
+import zero.maestro.model.Property;
 
 public class PropertySetterRenderer {
+
+    @Bean
+    private Property property;
 
     @Renderer(path = { "prop", "set" })
     public void render() throws ListPrinterException {
