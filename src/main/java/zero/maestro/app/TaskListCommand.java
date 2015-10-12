571e5a6abf9be6658176908cda3ea1011f9af36c
Rafael Monico <rmonico@moningrado>
2015-10-12 20:22:13 -0300

    task ls: --tags param to filter by tasks by tag

--- src/main/java/zero/maestro/app/TaskListCommand.java
+++ src/main/java/zero/maestro/app/TaskListCommand.java
@@ -7,13 +7,27 @@ import zero.easymvc.ArgumentsBean;
 import zero.easymvc.Bean;
 import zero.easymvc.CommandHandler;
 import zero.easymvc.Dependency;
+import zero.maestro.app.dao.TagDao;
 import zero.maestro.app.dao.TaskDao;
+import zero.maestro.app.dao.TaskTagDao;
+import zero.maestro.model.Tag;
 import zero.maestro.model.Task;
+import zero.maestro.model.TaskTag;
+
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.Where;
 
 public class TaskListCommand {
 
     @Dependency
     private TaskDao dao;
+
+    @Dependency
+    private TaskTagDao taskTagDao;
+
+    @Dependency
+    private TagDao tagDao;
 
     @ArgumentsBean
     private TaskListArguments args;
@@ -23,6 +37,27 @@ public class TaskListCommand {
 
     @CommandHandler(path = { "task", "ls" })
     public void execute() throws SQLException {
-        tasks = dao.queryForAll();
+        QueryBuilder<Task, Integer> taskBuilder = dao.queryBuilder();
+
+        String[] tags = args.getTags();
+        if (tags != null) {
+            QueryBuilder<Tag, Integer> tagBuilder = tagDao.queryBuilder();
+
+            Where<Tag, Integer> where = tagBuilder.where();
+
+            where.in(Tag.NAME_FIELD_NAME, (Object[]) tags);
+
+            QueryBuilder<TaskTag, Integer> taskTagBuilder = taskTagDao.queryBuilder();
+
+            taskTagBuilder.join(tagBuilder);
+
+            taskBuilder.join(taskTagBuilder);
+        }
+
+        taskBuilder.groupBy(Task.ID_FIELD_NAME);
+
+        PreparedQuery<Task> query = taskBuilder.prepare();
+
+        tasks = dao.query(query);
     }
 }
