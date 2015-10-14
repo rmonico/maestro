c5f72809c944d47edd3039de1b2261692ca287da
Rafael Monico <rmonico@moningrado>
2015-10-14 12:44:33 -0300

    task ls --columns test for default values from properties file

--- src/test/java/zero/maestrocli/TaskListTests.java
+++ src/test/java/zero/maestrocli/TaskListTests.java
@@ -126,7 +126,21 @@ public class TaskListTests extends MaestrocliTest {
 
     @Test
     public void should_pass_columns_param_to_renderer() throws EasyMVCException {
-        List<Object> beans = controller.run("task", "ls", "--columns=id,name");
+        List<Object> beans = controller.run("task", "ls", "--columns=id,name,supertask");
+
+        EasyMVCAssert.assertBeanList(beans, 2);
+
+        String[] columns = EasyMVCAssert.assertAndGetBean(beans, 1, String[].class);
+
+        assertEquals("Length", 3, columns.length);
+        assertEquals("id", columns[0]);
+        assertEquals("name", columns[1]);
+        assertEquals("supertask", columns[2]);
+    }
+
+    @Test
+    public void should_pass_columns_from_properties_when_parameter_is_not_defined() throws EasyMVCException {
+        List<Object> beans = controller.run("task", "ls");
 
         EasyMVCAssert.assertBeanList(beans, 2);
 
