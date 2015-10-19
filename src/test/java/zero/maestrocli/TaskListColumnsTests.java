93daf35a45905e0563926a243271aa0e3b42158b
Rafael Monico <rmonico@moningrado>
2015-10-18 23:26:21 -0200

    Extracted before method, new test: id column creation

--- src/test/java/zero/maestrocli/TaskListColumnsTests.java
+++ src/test/java/zero/maestrocli/TaskListColumnsTests.java
@@ -3,6 +3,7 @@ package zero.maestrocli;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import zero.easymvc.EasyMVCException;
@@ -10,16 +11,21 @@ import zero.utils.test.DBUnitDatasetFileNames;
 
 public class TaskListColumnsTests extends MaestrocliTest {
 
+    private SysoutWrapper wrapper;
+
+    @Before
+    public void setup() {
+        wrapper = new SysoutWrapper();
+        System.setOut(wrapper);
+    }
+
     @Test
     @DBUnitDatasetFileNames("dbunit/TaskListTests__should_render_every_possible_columns_for_top_tasks.xml")
-    public void should_render_every_possible_columns_for_top_tasks() throws EasyMVCException {
-        SysoutWrapper wrapper = new SysoutWrapper();
-        System.setOut(wrapper);
+    public void should_render_id_column_for_top_tasks() throws EasyMVCException {
+        controller.run("task", "ls", "--nosubtasks", "--columns=id");
 
-        controller.run("task", "ls", "--nosubtasks", "--columns=id,name,subtaskcount,tags,tagcount,attributesof:sometag,#othertag:someattr");
-
-        String expectedHeaderLine = "| ID | Name      | #Subtasks | Tags             | #Tags | #sometag                                               | #othertag:someattr                       |";
-        String expectedDataLine = "| #1 | Test task | 8         | sometag,othertag | 2     | [attr:value of attr in sometag on task #1;date:30/Mai] | Value of someattr in othertag on task #1 |";
+        String expectedHeaderLine = "| ID |";
+        String expectedDataLine = "| #1 |";
 
         assertTrue("Line count", wrapper.capturedLines.size() > 5);
         assertEquals("Header line", expectedHeaderLine, wrapper.capturedLines.get(1));
