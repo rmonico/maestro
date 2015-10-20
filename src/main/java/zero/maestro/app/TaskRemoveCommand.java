bb9011a8bcb4d8d3a754b6da9739ad282efe041b
Rafael Monico <rmonico@moningrado>
2015-10-20 14:01:48 -0200

    task rm command with tests (passing)

--- src/main/java/zero/maestro/app/TaskRemoveCommand.java
+++ src/main/java/zero/maestro/app/TaskRemoveCommand.java
@@ -1,15 +1,33 @@
 package zero.maestro.app;
 
+import java.sql.SQLException;
+
 import zero.easymvc.ArgumentsBean;
+import zero.easymvc.Bean;
 import zero.easymvc.CommandHandler;
+import zero.easymvc.Dependency;
+import zero.easymvc.EasyMVCException;
+import zero.maestro.app.dao.TaskDao;
+import zero.maestro.model.Task;
 
 public class TaskRemoveCommand {
 
     @ArgumentsBean
     private TaskRemoveArguments args;
 
-    @CommandHandler(path = { "task", "rm" })
-    public void execute() {
+    @Dependency
+    private TaskDao dao;
 
+    @Bean
+    private Task task;
+
+    @CommandHandler(path = { "task", "rm" })
+    public void execute() throws SQLException, EasyMVCException {
+        task = dao.queryForId(args.getTaskId());
+
+        if (task == null)
+            throw new EasyMVCException(String.format("No task with id #%d...", args.getTaskId()));
+
+        dao.delete(task);
     }
 }
--- src/main/java/zero/maestrocli/renderer/TaskRemoveRenderer.java
+++ src/main/java/zero/maestrocli/renderer/TaskRemoveRenderer.java
@@ -1,11 +1,17 @@
 package zero.maestrocli.renderer;
 
+import zero.easymvc.Bean;
 import zero.easymvc.Renderer;
+import zero.maestro.model.Task;
 
 public class TaskRemoveRenderer {
 
+    @Bean
+    private Task task;
+
     @Renderer(path = { "task", "rm" })
     public void render() {
-
+        String message = String.format("Task #%d (\"%s\") removed.", task.getId(), task.getName());
+        System.out.println(message);
     }
 }
