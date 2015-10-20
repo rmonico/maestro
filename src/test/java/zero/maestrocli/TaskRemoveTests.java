deba8110c50d11db51a9e5fa5beb00c862327004
Rafael Monico <rmonico@moningrado>
2015-10-20 12:00:05 -0200

    task rm tests (not passing)

---
+++ src/test/java/zero/maestrocli/TaskRemoveTests.java
@@ -0,0 +1,43 @@
+package zero.maestrocli;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+import zero.easymvc.EasyMVCException;
+import zero.utils.test.DBUnitDatasetFileNames;
+
+public class TaskRemoveTests extends MaestrocliTest {
+
+    @Test
+    @DBUnitDatasetFileNames("dbunit/TaskRemoveTests__should_remove_a_task.xml")
+    public void should_remove_a_task() throws EasyMVCException, SQLException, DatabaseUnitException, MalformedURLException {
+        controller.run("task", "rm", "1");
+
+        String connectionString = connectionManager.getConnectionString();
+        Connection jdbcConnection = DriverManager.getConnection(connectionString);
+
+        DatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+
+        IDataSet databaseDataSet = connection.createDataSet();
+        ITable actualTask = databaseDataSet.getTable("task");
+        ITable actualProperty = databaseDataSet.getTable("property");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dbunit/TaskRemoveTests__should_remove_a_task__expecteddata.xml"));
+        ITable expectedTask = expectedDataSet.getTable("task");
+        ITable expectedProperty = expectedDataSet.getTable("property");
+
+        Assertion.assertEquals(expectedTask, actualTask);
+        Assertion.assertEquals(expectedProperty, actualProperty);
+    }
+}
