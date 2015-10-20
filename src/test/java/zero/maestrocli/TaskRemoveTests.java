347158e181ced61d48a706540cb1d603313560d8
Rafael Monico <rmonico@moningrado>
2015-10-20 20:16:05 -0200

    task rm: Testing tasktag relationship removal

--- src/test/java/zero/maestrocli/TaskRemoveTests.java
+++ src/test/java/zero/maestrocli/TaskRemoveTests.java
@@ -1,5 +1,7 @@
 package zero.maestrocli;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.net.MalformedURLException;
 import java.sql.Connection;
@@ -19,17 +21,22 @@ import zero.utils.test.DBUnitDatasetFileNames;
 
 public class TaskRemoveTests extends MaestrocliTest {
 
-    @Test
-    @DBUnitDatasetFileNames("dbunit/TaskRemoveTests__should_remove_a_task.xml")
-    public void should_remove_a_task() throws EasyMVCException, SQLException, DatabaseUnitException, MalformedURLException {
-        controller.run("task", "rm", "1");
-
+    private IDataSet getDataset() throws SQLException, DatabaseUnitException {
         String connectionString = connectionManager.getConnectionString();
         Connection jdbcConnection = DriverManager.getConnection(connectionString);
 
         DatabaseConnection connection = new DatabaseConnection(jdbcConnection);
 
         IDataSet databaseDataSet = connection.createDataSet();
+        return databaseDataSet;
+    }
+
+    @Test
+    @DBUnitDatasetFileNames("dbunit/TaskRemoveTests__should_remove_a_task.xml")
+    public void should_remove_a_task() throws EasyMVCException, SQLException, DatabaseUnitException, MalformedURLException {
+        controller.run("task", "rm", "1");
+
+        IDataSet databaseDataSet = getDataset();
         ITable actualTask = databaseDataSet.getTable("task");
 
         IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dbunit/TaskRemoveTests__should_remove_a_task__expecteddata.xml"));
@@ -37,4 +44,22 @@ public class TaskRemoveTests extends MaestrocliTest {
 
         Assertion.assertEquals(expectedTask, actualTask);
     }
+
+    @Test
+    @DBUnitDatasetFileNames("dbunit/TaskRemoveTests__should_remove_task_and_its_tags.xml")
+    public void should_remove_task_and_its_tags() throws EasyMVCException, SQLException, DatabaseUnitException, MalformedURLException {
+        controller.run("task", "rm", "1");
+
+        IDataSet databaseDataSet = getDataset();
+        ITable actualTask = databaseDataSet.getTable("task");
+        ITable actualTaskTag = databaseDataSet.getTable("tasktag");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dbunit/TaskRemoveTests__should_remove_task_and_its_tags__expecteddata.xml"));
+        ITable expectedTask = expectedDataSet.getTable("task");
+
+        Assertion.assertEquals(expectedTask, actualTask);
+        assertEquals("TaskTag row count", 0, actualTaskTag.getRowCount());
+    }
+
+    // TODO Sub tasks remove tests
 }
