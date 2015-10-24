5b8801d9464919cc824df502c313f849ef3037f8
Rafael Monico <rmonico@moningrado>
2015-10-24 19:22:36 -0200

    task up tests (not passing)

---
+++ src/test/java/zero/maestrocli/TaskUpTests.java
@@ -0,0 +1,32 @@
+package zero.maestrocli;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.sql.SQLException;
+
+import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+import zero.easymvc.EasyMVCException;
+import zero.utils.test.DBUnitDatasetFileNames;
+
+public class TaskUpTests extends MaestrocliTest {
+
+    @Test
+    @DBUnitDatasetFileNames("dbunit/TaskUpTests__should_update_task_name.xml")
+    public void should_update_task_name() throws EasyMVCException, SQLException, DatabaseUnitException, MalformedURLException {
+        controller.run("task", "up", "2", "New task name");
+
+        IDataSet databaseDataSet = getDBUnitDataset();
+        ITable actualTask = databaseDataSet.getTable("task");
+
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("dbunit/TaskUpTests__should_update_task_name__expecteddata.xml"));
+        ITable expectedTask = expectedDataSet.getTable("task");
+
+        Assertion.assertEquals(expectedTask, actualTask);
+    }
+}
