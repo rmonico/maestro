7f2f48f113f989ed207e90cd2c971bc2a16e92a4
Rafael Monico <rmonico@moningrado>
2015-10-24 19:59:10 -0200

    task rm: invalid super task tests (not passing)

--- src/test/java/zero/maestrocli/TaskUpTests.java
+++ src/test/java/zero/maestrocli/TaskUpTests.java
@@ -1,5 +1,11 @@
 package zero.maestrocli;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.fail;
+
 import java.io.File;
 import java.net.MalformedURLException;
 import java.sql.SQLException;
@@ -42,6 +48,19 @@ public class TaskUpTests extends MaestrocliTest {
         ITable expectedTask = expectedDataSet.getTable("task");
 
         Assertion.assertEquals(expectedTask, actualTask);
+    }
+
+    @Test
+    @DBUnitDatasetFileNames("dbunit/TaskUpTests__should_update_task_name.xml")
+    public void should_try_update_to_a_invalid_supertask() {
+        try {
+            controller.run("task", "up", "2", "--supertaskid=45");
+            fail("Exception expected");
+        } catch (Throwable e) {
+            assertThat(e, is(notNullValue()));
+            assertThat(e, instanceOf(RuntimeException.class));
+            assertThat(e.getMessage(), is("Supertask id #45 not found."));
+        }
     }
 
 }
