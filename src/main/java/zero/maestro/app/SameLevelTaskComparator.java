fa3e3cb3ff1d6003a17d3b757eda2497f75203a2
Rafael Monico <rmonico@moningrado>
2015-12-06 16:59:16 -0200

    Sorting subtasks by name and root tasks by reversed id (tests passing)

--- src/main/java/zero/maestro/app/SameLevelTaskComparator.java
+++ src/main/java/zero/maestro/app/SameLevelTaskComparator.java
@@ -11,11 +11,18 @@ public class SameLevelTaskComparator implements Comparator<Task> {
         // -1: task < anotherTask
         // 0: task = anotherTask
         // +1: task > anotherTask
-        
-        int taskId = task.getId();
-        int anotherTaskId = anotherTask.getId();
 
-        return anotherTaskId - taskId;
+        if (task.getSuperTask() == null) {
+            int taskId = task.getId();
+            int anotherTaskId = anotherTask.getId();
+
+            return anotherTaskId - taskId;
+        } else {
+            String taskName = task.getName();
+            String anotherTaskName = anotherTask.getName();
+
+            return taskName.compareTo(anotherTaskName);
+        }
     }
 
 }
