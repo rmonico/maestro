84140db9c0e13ba233d1511d2e152c2652d36c92
Rafael Monico <rmonico@moningrado>
2016-03-11 21:02:24 -0300

    Atividade principal movida para outro pacote

--- app/src/main/java/br/maestro/br/maestro/activities/mainactivity/MainActivity.java
+++ app/src/main/java/br/maestro/br/maestro/activities/mainactivity/MainActivity.java
@@ -1,4 +1,4 @@
-package br.maestro;
+package br.maestro.br.maestro.activities.mainactivity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.maestro.R;
 
 public class MainActivity extends AppCompatActivity {
 
