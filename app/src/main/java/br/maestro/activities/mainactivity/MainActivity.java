2a7222cbe6ce69009af7717d055873123ed07bdc
Rafael Monico <rmonico@moningrado>
2016-03-12 21:27:37 -0300

    Adicionada ToolBar no modo de compatibilidade

--- app/src/main/java/br/maestro/activities/mainactivity/MainActivity.java
+++ app/src/main/java/br/maestro/activities/mainactivity/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -28,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
 //        setContentView(R.layout.mainactivity);
 //
 //        initializeControls();
