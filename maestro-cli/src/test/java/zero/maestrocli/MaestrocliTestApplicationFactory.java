c7eb29edbba1aef86ce458b176a0c75eee92a09a
Rafael Monico <rmonico@moningrado>
2016-04-05 19:48:22 -0300

    MaestrocliTestApplicationFactory not inheriting MaestroApplicationFactory anymore

--- maestro-cli/src/test/java/zero/maestrocli/MaestrocliTestApplicationFactory.java
+++ maestro-cli/src/test/java/zero/maestrocli/MaestrocliTestApplicationFactory.java
@@ -1,12 +1,16 @@
 package zero.maestrocli;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.j256.ormlite.support.ConnectionSource;
 
+import ch.qos.logback.classic.Logger;
+import zero.easymvc.EasyMVC;
 import zero.easymvc.ormlite.ConnectionManager;
+import zero.easymvc.ormlite.DaoManager;
 import zero.easymvc.ormlite.DatabaseUpdater;
 import zero.easymvc.ormlite.MetaInfUpdater;
 import zero.maestro.app.MaestroApplicationFactory;
@@ -14,23 +18,23 @@ import zero.maestro.database.DatabaseVersion_1;
 import zero.utils.test.AbstractTestApplicationFactory;
 import zero.utils.test.TestApplicationFactory;
 
-public class MaestrocliTestApplicationFactory extends MaestrocliApplicationFactory implements TestApplicationFactory {
+public class MaestrocliTestApplicationFactory implements TestApplicationFactory {
 
     private AbstractTestApplicationFactory testApplicationFactoryDelegated;
+    private MaestrocliApplicationFactory maestrocliApplicationFactoryDelegated;
 
     public MaestrocliTestApplicationFactory() {
         this(AbstractTestApplicationFactory.DATABASE_LAST_VERSION);
     }
 
     public MaestrocliTestApplicationFactory(int databaseVersion) {
-        super(MaestroApplicationFactory.BASENAME + "_test");
+        maestrocliApplicationFactoryDelegated = new MaestrocliApplicationFactory(MaestroApplicationFactory.BASENAME + "_test");
 
         testApplicationFactoryDelegated = new AbstractTestApplicationFactory(databaseVersion);
     }
 
-    @Override
     public ConnectionManager makeConnectionManager() throws SQLException {
-        ConnectionManager connectionManager = super.makeConnectionManager();
+        ConnectionManager connectionManager = maestrocliApplicationFactoryDelegated.makeConnectionManager();
 
         ConnectionSource connection = connectionManager.getConnection();
 
@@ -42,6 +46,26 @@ public class MaestrocliTestApplicationFactory extends MaestrocliApplicationFacto
         testApplicationFactoryDelegated.addUpdaters(updaters);
 
         return connectionManager;
+    }
+
+    @Override
+    public void makeProperties() throws IOException {
+        maestrocliApplicationFactoryDelegated.makeProperties();
+    }
+
+    @Override
+    public Logger makeLogger() {
+        return maestrocliApplicationFactoryDelegated.makeLogger();
+    }
+
+    @Override
+    public DaoManager makeDaoManager() {
+        return maestrocliApplicationFactoryDelegated.makeDaoManager();
+    }
+
+    @Override
+    public EasyMVC makeController() throws SQLException {
+        return maestrocliApplicationFactoryDelegated.makeController();
     }
 
     @Override
